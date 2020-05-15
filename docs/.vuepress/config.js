b948f7a25d3a5ffd79749aef6ff30227be5d37fe
vachetvirginie <vachetvirginie@gmail.com>
2020-05-15 10:34:02 +0200

    test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,84 +1,65 @@
 module.exports = {
     title: 'Waspy \'s brain',
     description: 'Waspy \'s brain',
-    // locales: {
-    //     '/': {
-    //         lang: 'en-US',
-    //         title: 'Waspy \'s brain',
-    //         description: 'doc',
-    //     },
-    //     '/fr/': {
-    //         lang: 'fr-FR',
-    //         title: 'Waspy \'s brain',
-    //         description: 'ma doc',
-    //     },
-    // },
     themeConfig: {
-        docsDir: 'docs',
-        locales: {
-            '/': {
-                label: 'English',
-                //  selectText: 'Languages',
-                sidebar: {
-                    '/divers/': [{
-                        title: 'Divers',
-                        collapsable: false,
-                        children: ['LiensUtiles', 'Emojis in VuePress markdown'],
-                    }, ],
-                    '/docker/': [{
-                        title: 'Docker',
-                        collapsable: false,
-                        children: ['Commandes de base'],
-                    }, ],
-                    '/git/': [{
-                        title: 'Git',
-                        collapsable: false,
-                        children: ['alias', 'Commandes de base'],
-                    }, ],
-                    '/symfony/': [{
-                        title: 'Symfony',
-                        collapsable: false,
-                        children: ['Best Practices SF4'],
-                    }, ],
-                    '/': [''], // fallback
-                },
-                nav: [
-                    { text: '', link: '/' },
-                    { text: 'Divers', link: '/divers/' },
-                    { text: 'Docker', link: '/docker/' },
-                    { text: 'Git', link: '/git/' },
-                    { text: 'Symfony', link: '/symfony/' },
-                    { text: 'Github', link: 'https://github.com/VachetVirginie/vuePressTest' },
-                ],
-            },
-            // '/fr/': {
-            //     label: 'Français',
-            //     selectText: 'Langues',
-            //     sidebar: {
-            //         '/fr/docker/': [{
-            //             title: 'Mon chien - docker',
-            //             collapsable: false,
-            //             children: ['food', 'hobbies'],
-            //         }, ],
-            //         '/fr/symfony/': [{
-            //             title: 'Mon chat - symfony',
-            //             collapsable: false,
-            //             children: ['food', 'hobbies'],
-            //         }, ],
-            //         '/fr/git/': [{
-            //             title: 'Mon lapin - git',
-            //             collapsable: false,
-            //             children: ['food', 'hobbies'],
-            //         }, ],
-            //         '/': [''], // fallback
-            //     },
-            //     nav: [
-            //         { text: 'docker', link: '/fr/docker/' },
-            //         { text: 'symfony', link: '/fr/symfony/' },
-            //         { text: 'git', link: '/fr/git/' },
-            //         { text: 'Github', link: 'https://github.com/VachetVirginie/vuePressTest' },
-            //     ],
-            // },
+        sidebar: {
+            '/divers/': [{
+                title: 'Divers',
+                collapsable: false,
+                children: ['LiensUtiles', 'Emojis in VuePress markdown'],
+            }, ],
+            '/docker/': [{
+                title: 'Docker',
+                collapsable: false,
+                children: ['Commandes de base'],
+            }, ],
+            '/git/': [{
+                title: 'Git',
+                collapsable: false,
+                children: ['alias', 'Commandes de base'],
+            }, ],
+            '/symfony/': [{
+                title: 'Symfony',
+                collapsable: false,
+                children: ['Best Practices SF4'],
+            }, ],
+            '/': [''], // fallback
         },
+        nav: [
+            { text: '', link: '/' },
+            { text: 'Divers', link: '/divers/' },
+            { text: 'Docker', link: '/docker/' },
+            { text: 'Git', link: '/git/' },
+            { text: 'Symfony', link: '/symfony/' },
+            { text: 'Github', link: 'https://github.com/VachetVirginie/vuePressTest' },
+        ],
     },
+    // '/fr/': {
+    //     label: 'Français',
+    //     selectText: 'Langues',
+    //     sidebar: {
+    //         '/fr/docker/': [{
+    //             title: 'Mon chien - docker',
+    //             collapsable: false,
+    //             children: ['food', 'hobbies'],
+    //         }, ],
+    //         '/fr/symfony/': [{
+    //             title: 'Mon chat - symfony',
+    //             collapsable: false,
+    //             children: ['food', 'hobbies'],
+    //         }, ],
+    //         '/fr/git/': [{
+    //             title: 'Mon lapin - git',
+    //             collapsable: false,
+    //             children: ['food', 'hobbies'],
+    //         }, ],
+    //         '/': [''], // fallback
+    //     },
+    //     nav: [
+    //         { text: 'docker', link: '/fr/docker/' },
+    //         { text: 'symfony', link: '/fr/symfony/' },
+    //         { text: 'git', link: '/fr/git/' },
+    //         { text: 'Github', link: 'https://github.com/VachetVirginie/vuePressTest' },
+    //     ],
+    // },
 };
