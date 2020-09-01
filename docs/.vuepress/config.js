8f2aa980036f0587bb6daf61b19297bd8602363d
Vivi <vachetvirginie@gmail.com>
2020-09-01 14:01:07 +0200

    fix config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     title: 'Vivi \'s brain',
     description: 'Vivi \'s brain',
+    plugins: ['@vuepress/back-to-top', '@vuepress/nprogress'],
     themeConfig: {
         logo: '/assets/img/logo.png',
         searchPlaceholder: 'Search...',
